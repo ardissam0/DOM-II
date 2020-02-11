b5156c8ec283ce2f2abfa1b436d9a4e5ac7d9b34
Sam Ardis <ardis.productions@gmail.com>
2020-02-11 17:06:04 -0500

    Added half of mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,53 @@
 // Your code goes here
+
+const logoChange = document.querySelector('.logo-heading');
+
+logoChange.addEventListener('wheel', () => {
+    
+});
+
+const navChange = document.querySelectorAll('.nav-link').forEach(el => {
+    el.addEventListener('click', function() {
+        el.style.color = "Coral";
+    });
+})
+
+const headerImg = document.querySelector('.intro img');
+
+headerImg.addEventListener('mouseover', () => {
+    headerImg.style.transform = "scale(1.3)";
+    headerImg.style.transition = "transform 1s";
+});
+
+headerImg.addEventListener('mouseout', () => {
+    headerImg.style.transform = "scale(1)";
+    headerImg.style.transition = "transform 1s";
+});
+
+
+const topContentImg = document.querySelector('.content-section .img-content');
+
+topContentImg.addEventListener('mouseenter', () => {
+    topContentImg.style.transform = "scale(1.25)";
+    topContentImg.style.transition = "transform 1.5s";
+});
+
+topContentImg.addEventListener('mouseleave', () => {
+    topContentImg.style.transform = "scale(1)";
+    topContentImg.style.transition = "transform 1.25s";
+});
+
+const inverseContentImg = document.querySelector('.inverse-content .img-content');
+
+inverseContentImg.addEventListener('mousemove', () => {
+    inverseContentImg.style.transform = "scale(1.2)";
+    inverseContentImg.style.transition = "transform 1s";
+});
+
+const bottomImg = document.querySelector('.content-destination img');
+
+bottomImg.addEventListener('dblclick', () => {
+    bottomImg.style.transform = "scale(1.2)";
+    bottomImg.style.transition = "transform 1s";
+});
+
